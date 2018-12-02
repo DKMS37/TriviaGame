65d6a3985a51eb03b22760d75a144ac1d9d3dad4
dkms <john_dk@sbcglobal.net>
2018-12-02 11:37:19 -0800

    making a question list

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
  // start the game when user clicks on Start button
  $("#start-button").on("click", gameStart.startTimer);
- });
 
+ 
 
  var gameStart = {
     // set the time at 60 seconds, and count down by 1 second
@@ -15,6 +15,7 @@ $(document).ready(function(){
       $("#start-page").hide();
 
     },
+
     countdown: function() {
         gameStart.timeRemaining--;
         $("#timer").text("Time Remaining: "+ gameStart.timeRemaining);
@@ -32,7 +33,49 @@ $(document).ready(function(){
         clearInterval();
         
     },
+// array of objects with the questions, possible answers, and the correct answer
+    questionList = [{
+        question: "What kind of animal is a Komodo Dragon?",
+        answer: ["Bird", "Snake", "Dog", "Lizard"],
+        correct: "Lizard"
+    },
+    {   question: "Which Bird has the largest wingspan of any living bird?",
+        answer: ["American Eagle", "Wandering Albatros", "Pelican", "Taiwanese Pigeon"],
+        correct: "Wandering Albatros"
+    },
+    {   quetions: "What kind of creature is a Portuguese Man O War?",
+        answer: ["Dog", "Cat", "Horse", "Jellyfish"],
+        correct: "Jellyfish"
+    },
+    {   question: "Which creatures produce Gossamer?",
+        answer: ["Snakes", "Worms", "Spiders", "Scorpions"],
+        correct: "Spiders"
+    },
+    {   question: "From which animal is a Mohair obtained?",
+        answer: ["Angora Goat", "Stalion Horse", "Zebra", "Monkey"],
+        correct: "Angora Goat"
+    },
+    {   question: "How many Arms do most Starfish have?",
+        answer: ["Six", "Five", "Two", "Ten"],
+        correct: "Five"
+    },
+    {   question: "What is a Cabbage White?",
+        answer: ["Rabbit", "Deer", "Turtle", "Butterfly"],
+        correct: "Butterfly"
+    },
+    {   question: "What is the scientific name for the trunk of an Elephant?",
+        answer: ["Canis Rufus", "Alouatta", "Proboscis", "Esox"],
+        correct: "Proboscis"
+    },
+    {   question: "Murder is the collective noun for a group of which Bird?",
+        answer: ["Owls", "Vultures", "Kingfishers", "Crows"],
+        correct: "Crows"
+    },
+    {   question: "Which Ape gets its name from the Malay word meaning Man of the Forest?",
+        answer: ["Orangutan", "Gorilla", "Chimpanzee", "Gibbon"],
+        correct: "Orangutan"
+    }],
 
 
 
-};
+};});
