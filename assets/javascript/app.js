b5f3c7e7cf7282144f12454780d7273b5d7d194f
dkms <john_dk@sbcglobal.net>
2018-12-03 17:40:37 -0800

    finishing up the trivia game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,64 @@
 $(document).ready(function(){
-
- // start the game when user clicks on Start button.
+  //hide the end page with results.
+  $(".hidden").hide();
+ //start the game when user clicks on Start button.
  $("#start-button").on("click", gameStart.startTimer);
 
  });
 
  var gameStart = {
-    // set the time at 60 seconds, and count down by 1 second.
+    //set the time at 60 seconds, and count down by 1 second.
     timeRemaining : 60,
     // start the timer, hide the start page, show the questions.
-    startTimer: function() {
-      $("#timer").text("Time Remaining: "+ gameStart.timeRemaining);
+    startTimer: function(){
+      $("#timer").text("Time Remaining: " + gameStart.timeRemaining);
       setInterval(gameStart.countdown, 1000);
       $("#start-page").hide();
       trivia.displayQuestions();
     },
-
-    countdown: function() {
+   //decrement the timer and update the user interface, stop the timer at 0.
+    countdown: function(){
         gameStart.timeRemaining--;
         $("#timer").text("Time Remaining: "+ gameStart.timeRemaining);
         if (gameStart.timeRemaining === 0){
           gameStart.stopTimer();
           $("#timer").empty();
-          $("#start-page").show();
-          $("#timer").hide();
-        }   
+        
+     }
+    
     },
     
-    // stop the timer and reset.
+    //stop the timer and check the answers.
     stopTimer: function(){
-        clearInterval();   
+        clearInterval();
+        trivia.checkAnswers();
     },
-};
-
+     //hide the quetions and display the end page with results.
+    showEndPage: function(numCorrect, numIncorrect, numUnanswered){
+      $("#end-page").show();
+      $("#questions-box").empty();
+      $("#timer").empty();
+      $("#timer").hide();
+      $("#correct-answers").text("Correct answers: " + numCorrect);
+      $("#incorrect-answers").text("Incorrect answers: " + numIncorrect);
+      $("#unanswered").text("Skipped questions: " + numUnanswered);
+    }
+}
 
     
-    var trivia = {
-        // pull questions from the array of questions, loop through them,and append to user interface.
-    displayQuestions: function(){
-        var divContainer = $("#questions-box");
-        divContainer.append('<h2>Answer the following questions:<h2>');
-        for (var i = 0; i < questionList.length; i++){
+      var trivia = {
+      //pull questions from the array of questions, loop through them,and append to user interface.
+      displayQuestions: function(){
+         var divContainer = $("#questions-box");
+         divContainer.append('<h2>Answer the following questions:<h2>');
+         for (var i = 0; i < questionList.length; i++){
             divContainer.append('<div id="question">' + questionList[i].question + '</div>');
 
             var answer1 = questionList[i].answers[0];
             var answer2 = questionList[i].answers[1];
             var answer3 = questionList[i].answers[2];
             var answer4 = questionList[i].answers[3];
-            
+            //append the Trivia Multiple questions page with four answers to choose from.
             divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="form-check-label-answers" id="radio'+i+'label" for="radio'+i+'">' + answer1 + '</label></div>');
   
             divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="form-check-label-answers" id="radio'+i+'label" for="radio'+i+'">' + answer2 + '</label></div>');
@@ -57,17 +68,44 @@ $(document).ready(function(){
             divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="form-check-label-answers" id="radio'+i+'label" for="radio'+i+'">' + answer4 + '</label></div>');
         
     
-        };
-
+         };
+        //add a Done button to the end of the page and register its click handler.
            var doneButton = '<button class="btn btn-primary" id="done-button" type="submit">Done</button>';
            divContainer.append(doneButton);
-           $("#done-button").on("click", gameState.stopTimer);
+           $("#done-button").on("click", gameStart.stopTimer);
+            
+        },
     
-      },
-    }
+        //test if the user answers are correct, incorrect, or if there are unanswered questions.
+        checkAnswers: function(){
+        var correctAnswer;
+        var userAnswer;
+        var numCorrect = 0;
+        var numIncorrect = 0;
+        var numUnanswered = 0;
+        //loop through to compare the text of the label with the user answers and increment score counts.
+        for (var i = 0; i < questionList.length; i++){
+        correctAnswer = questionList[i].correct;
+        userAnswer = $('input[id=radio'+i+']:checked + label').text();
+        console.log(i);
+        if (userAnswer === correctAnswer){
+            numCorrect++;
+        } else if (userAnswer === ""){
+            numUnanswered++;
+        } else if (userAnswer !== correctAnswer){
+            { numIncorrect++;}
 
-// array of questions, possible answers, and the correct answer
-   var questionList = [
+        }
+     }
+        //show the end page with the score tally.
+        gameStart.showEndPage( numCorrect, numIncorrect, numUnanswered);
+    },
+ }
+
+
+   //array of questions, possible answers, and the correct answer.
+   var questionList = 
+   [
        
     {   question: "What kind of animal is a Komodo Dragon?",
         answers: ["Bird", "Snake", "Dog", "Lizard"],
@@ -86,7 +124,7 @@ $(document).ready(function(){
         correct: "Spiders"
     },
     {   question: "From which animal is a Mohair obtained?",
-        answers: ["Angora Goat", "Stalion Horse", "Zebra", "Monkey"],
+        answers: ["Angora Goat", "Stallion Horse", "Zebra", "Monkey"],
         correct: "Angora Goat"
     },
     {   question: "How many Arms do most Starfish have?",
@@ -108,7 +146,8 @@ $(document).ready(function(){
     {   question: "Which Ape gets its name from the Malay word meaning Man of the Forest?",
         answers: ["Orangutan", "Gorilla", "Chimpanzee", "Gibbon"],
         correct: "Orangutan"
-    }]
+    }
+]
 
-
+    
 
